577566395a6b4442bbfb195112d163a31274a919
Kübra Eser <kubra90000@gmail.com>
2026-04-14 09:08:21 +0300

    Added result

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,27 +1,43 @@
 package org.example;
 
+
+
 public class Main {
+
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
 
     public static boolean shouldWakeUp(boolean isBarking, int clock) {
-        
+        return clock >= 0 && clock <= 23 &&
+                isBarking &&
+                (clock < 8 || clock >= 20);
     }
 
     public static boolean hasTeen(int firstAge, int secondAge, int thirdAge) {
-       
+        return (firstAge >= 13 && firstAge <= 19) ||
+                (secondAge >= 13 && secondAge <= 19) ||
+                (thirdAge >= 13 && thirdAge <= 19);
     }
 
     public static boolean isCatPlaying(boolean isSummer, int temp) {
-     
+        return (temp >= 25 && temp <= 35) ||
+                (isSummer && temp >= 25 && temp <= 45);
     }
 
     public static double area(double width, double height) {
-       
+       if (width< 0 || height <0){
+           return  -1;
+       }
+         return  width*height;
+
     }
 
     public static double area(double radius) {
-       
+       if(radius <0){
+           return  -1;
+       }
+       return  radius*radius*Math.PI;
     }
 }
